a7d9ca8a6eebec8d0b0008c88d33254b775c1e2a
rnimisha <nimisaraut.gmail.com>
2022-09-27 01:15:55 +0545

    hover animation

--- client/src/Components/IndividualRoom/IndividualRoom.js
+++ client/src/Components/IndividualRoom/IndividualRoom.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { RoomBox, RoomImg, RoomName, Info, Price } from './IndividualRoomStyled'
+import { RoomBox, RoomImg, RoomName, Info, Price, RoomImgContainer } from './IndividualRoomStyled'
 
 const IndividualRoom = ({ image, price, name }) => {
   return (
     <RoomBox>
-        <RoomImg src={image} alt="" />
+        <RoomImgContainer>
+            <RoomImg src={image} alt="" />
+            <Price>Rs.{price}</Price>
+        </RoomImgContainer>
         <RoomName>{name}</RoomName>
         <Info>3 people, 3 bedrooms</Info>
-        <Price>Rs.{price}</Price>
     </RoomBox>
   )
 }
--- client/src/Components/IndividualRoom/IndividualRoomStyled.js
+++ client/src/Components/IndividualRoom/IndividualRoomStyled.js
@@ -7,18 +7,37 @@ export const RoomBox = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1rem;
-    position: relative;
+    overflow: hidden;
 `
-export const RoomImg = styled.img`
+export const RoomImgContainer = styled.div`
+    position: relative;
     width: 100%;
     height: 70%;
+    overflow: hidden;
+`
 
+export const RoomImg = styled.img`
+    cursor: pointer;
+    width: 100%;
+    height: 100%;
+    transform: scale(1.11);
+    transition: ease-in-out .40s;
+
+    &:hover{
+        transform: scale(1);
+    }
 `
 
 export const RoomName = styled.h2`
     font-size: 2rem;
     color: #877147;
     font-weight: 300;
+    cursor: pointer;
+    position: relative;
+
+    &:hover{
+        color : #0c6354;
+    }
 `
 
 export const Info = styled.p`
@@ -30,7 +49,7 @@ export const Info = styled.p`
 export const Price = styled.div`
     position: absolute;
     left: 0;
-    top: 50%;
+    bottom: 15%;
     background-color: #877147;
     color: #f7f7f7;
     width: 40%;

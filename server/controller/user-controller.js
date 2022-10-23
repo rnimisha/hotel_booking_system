eff9b7e9a4c9e5f3b1680b35eaad1fde2879f3ed
rnimisha <nimisaraut@gmail.com>
2022-10-23 23:41:33 +0545

    hash password usign bcrypt

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,10 +1,19 @@
 import userModel from '../model/User.js'
+import bcrypt from 'bcrypt'
 
 export const registerUser = async (req, res) => {
+  const saltRounds = 10
   try {
-    await userModel.create(req.body)
-    res.json({
-      success: true
+    bcrypt.hash(req.body.password, saltRounds, async function (err, hash) {
+      if (err) {
+        throw new Error(err.message)
+      } else {
+        req.body.password = hash
+        await userModel.create(req.body)
+        res.json({
+          success: true
+        })
+      }
     })
   } catch (err) {
     res.status(400).json({
